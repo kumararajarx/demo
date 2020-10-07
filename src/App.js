04ab5f3f8e76af22c548a92fd99f587b67afd4c9
kumararaja <kumararaja@infinijith.com>
2020-10-07 11:00:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { createStore } from "redux";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
+
+// store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  );
+}
+
+export default App;
